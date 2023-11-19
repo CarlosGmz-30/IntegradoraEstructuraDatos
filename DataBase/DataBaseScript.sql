33bcdf981dc181d46b69361a033fe0a5caffe888
LeonardoNoriega <20213tn126@utez.edu.mx>
2023-11-18 20:16:58 -0600

    fix(sql):
    corregi el tamanio de la columna descripcion a 150

--- DataBase/DataBaseScript.sql
+++ DataBase/DataBaseScript.sql
@@ -6,7 +6,7 @@ create table tarea
     id_tarea    int auto_increment
         primary key,
     titulo      varchar(32) not null,
-    descripcion varchar(32) null,
+    descripcion varchar(150) null,
     prioridad   varchar(10)  null,
     estado      varchar(16) null
 );
@@ -14,7 +14,7 @@ create table tarea
 DELIMITER $$
 CREATE PROCEDURE agregar_tarea(
     IN titulo VARCHAR(32),
-    IN descripcion VARCHAR(32),
+    IN descripcion VARCHAR(150),
     IN prioridad VARCHAR(10),
     IN estado VARCHAR(16))
 BEGIN
