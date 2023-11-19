7616f4d7a3ca67b42185a5e170f6b0786cc1c44d
LeonardoNoriega <20213tn126@utez.edu.mx>
2023-11-18 21:10:11 -0600

    feat(sql):
    Agregue atributo fecha con tamanio 50 para posteriores funcionalidades con la organizacion de la cola por prioridad

--- DataBase/DataBaseScript.sql
+++ DataBase/DataBaseScript.sql
@@ -3,32 +3,33 @@ CREATE DATABASE TAREAS;
 USE TAREAS;
 create table tarea
 (
-    id_tarea    int auto_increment
-        primary key,
-    titulo      varchar(32) not null,
-    descripcion varchar(150) null,
-    prioridad   varchar(10)  null,
-    estado      varchar(16) null
+    id_tarea    int auto_increment primary key,
+    titulo      varchar(32)  not null,
+    descripcion varchar(150) not null,
+    fecha       varchar(50)  not null,
+    prioridad   varchar(10)  not null,
+    estado      varchar(16)  not null default 'pendiente'
 );
 
 DELIMITER $$
 CREATE PROCEDURE agregar_tarea(
     IN titulo VARCHAR(32),
     IN descripcion VARCHAR(150),
+    IN fecha VARCHAR(50),
     IN prioridad VARCHAR(10),
     IN estado VARCHAR(16))
 BEGIN
-    INSERT INTO tarea(titulo,descripcion,prioridad,estado)
+    INSERT INTO tarea(titulo, descripcion, fecha, prioridad, estado)
     VALUES (titulo,
             descripcion,
+            fecha,
             prioridad,
-            estado
-           );
+            estado);
 END
 $$
 DELIMITER ;
 
-call agregar_tarea('Investigacion','Investigacion sobre JWT','media','pendiente');
+call agregar_tarea('Investigacion', 'Investigacion sobre JWT', '17 de Junio', 'media', 'pendiente');
 
 select *
 from tarea;
